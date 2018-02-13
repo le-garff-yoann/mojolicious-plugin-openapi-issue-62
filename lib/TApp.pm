614dce43a0d64ee685d21f8a448c93f3338b78d9
le-garff-yoann <pe.weeble@yahoo.fr>
2018-02-13 12:03:11 +0000

    feat(404 handling) remove the before_render hook

--- lib/TApp.pm
+++ lib/TApp.pm
@@ -9,24 +9,6 @@ sub startup {
 
     $self->helper('tapp.openapi.url' => sub { state $p = shift->app->home->child('share', 't-app.json') });
 
-    $self->hook(before_render => sub {
-        my ($c, $args) = @_;
-
-        my $err;
-
-        my $tpl = $args->{template} // '';
-
-        if ($tpl eq 'exception') {
-            $err = $c->stash('exception')->message;
-        } elsif ($tpl eq 'not_found') {
-            $err = "the page you were looking for doesn't exist."
-        } else {
-            return;
-        }
-
-        $args->{json} = { error => $err };
-    });
-
     $self->plugin(OpenAPI => {
         url => $self->tapp->openapi->url
     });
